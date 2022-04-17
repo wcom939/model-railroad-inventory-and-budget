914e75725fd9ea0bb0ab4d2b5ce4be6b672a6d5a
wcom939 <dlfourman@gmail.com>
2022-04-17 19:00:28 -0400

    setting up the shell menu

--- ModelRailroadBudgetAndInventory/App.xaml.cs
+++ ModelRailroadBudgetAndInventory/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
+using Windows.ApplicationModel.Core;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -29,72 +30,50 @@ namespace ModelRailroadBudgetAndInventory
         public App()
         {
             this.InitializeComponent();
-            this.Suspending += OnSuspending;
         }
 
-        /// <summary>
-        /// Invoked when the application is launched normally by the end user.  Other entry points
-        /// will be used such as when the application is launched to open a specific file.
-        /// </summary>
-        /// <param name="e">Details about the launch request and process.</param>
+        /// <inheritdoc/>
         protected override void OnLaunched(LaunchActivatedEventArgs e)
         {
-            Frame rootFrame = Window.Current.Content as Frame;
-
-            // Do not repeat app initialization when the Window already has content,
-            // just ensure that the window is active
-            if (rootFrame == null)
+            // Ensure the UI is initialized
+            if (Window.Current.Content is null)
             {
-                // Create a Frame to act as the navigation context and navigate to the first page
-                rootFrame = new Frame();
+                Window.Current.Content = new Shell();
+                /*
+                TitleBarHelper.StyleTitleBar();
+                TitleBarHelper.ExpandViewIntoTitleBar();
 
-                rootFrame.NavigationFailed += OnNavigationFailed;
-
-                if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
-                {
-                    //TODO: Load state from previously suspended application
-                }
-
-                // Place the frame in the current Window
-                Window.Current.Content = rootFrame;
+                // Register services
+                Ioc.Default.ConfigureServices(
+                    new ServiceCollection()
+                        .AddSingleton<IDialogService, DialogService>() //Services
+                        .AddSingleton<IFilesService, FilesService>()
+                        .AddSingleton<ISettingsService, SettingsService>()
+                        .AddSingleton(RestService.For<IRedditService>("https://www.reddit.com/"))
+                        .AddSingleton(RestService.For<IContactsService>("https://randomuser.me/"))
+                        .AddTransient<PostWidgetViewModel>() //ViewModels
+                        .AddTransient<SubredditWidgetViewModel>()
+                        .AddTransient<ContactsListWidgetViewModel>()
+                        .AddTransient<AsyncRelayCommandPageViewModel>()
+                        .AddTransient<IocPageViewModel>()
+                        .AddTransient<MessengerPageViewModel>()
+                        .AddTransient<ObservableObjectPageViewModel>()
+                        .AddTransient<ObservableValidatorPageViewModel>()
+                        .AddTransient<ValidationFormWidgetViewModel>()
+                        .AddTransient<RelayCommandPageViewModel>()
+                        .AddTransient<CollectionsPageViewModel>()
+                        .AddTransient<SamplePageViewModel>()
+                        .BuildServiceProvider());*/
             }
 
+            // Enable the prelaunch if needed, and activate the window
             if (e.PrelaunchActivated == false)
             {
-                if (rootFrame.Content == null)
-                {
-                    // When the navigation stack isn't restored navigate to the first page,
-                    // configuring the new page by passing required information as a navigation
-                    // parameter
-                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
-                }
-                // Ensure the current window is active
+                CoreApplication.EnablePrelaunch(true);
+
                 Window.Current.Activate();
             }
         }
-
-        /// <summary>
-        /// Invoked when Navigation to a certain page fails
-        /// </summary>
-        /// <param name="sender">The Frame which failed navigation</param>
-        /// <param name="e">Details about the navigation failure</param>
-        void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
-        {
-            throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
-        }
-
-        /// <summary>
-        /// Invoked when application execution is being suspended.  Application state is saved
-        /// without knowing whether the application will be terminated or resumed with the contents
-        /// of memory still intact.
-        /// </summary>
-        /// <param name="sender">The source of the suspend request.</param>
-        /// <param name="e">Details about the suspend request.</param>
-        private void OnSuspending(object sender, SuspendingEventArgs e)
-        {
-            var deferral = e.SuspendingOperation.GetDeferral();
-            //TODO: Save application state and stop any background activity
-            deferral.Complete();
-        }
     }
+
 }
---
+++ ModelRailroadBudgetAndInventory/Shell.xaml.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using NavigationView = Microsoft.UI.Xaml.Controls.NavigationView;
+using NavigationViewBackRequestedEventArgs = Microsoft.UI.Xaml.Controls.NavigationViewBackRequestedEventArgs;
+using NavigationViewItemInvokedEventArgs = Microsoft.UI.Xaml.Controls.NavigationViewItemInvokedEventArgs;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace ModelRailroadBudgetAndInventory
+{
+    public sealed partial class Shell : UserControl
+    {
+        private readonly IReadOnlyCollection<SampleEntry> NavigationItems;
+
+        public Shell()
+        {
+            this.InitializeComponent();
+
+            /*NavigationItems = new[]
+            {
+            new SampleEntry(IntroductionItem, typeof(IntroductionPage)),
+            new SampleEntry(ObservableObjectItem, typeof(ObservableObjectPage), "ObservableObject", "observable inotify property changed propertychanging changing"),
+            new SampleEntry(ObservableValidatorItem, typeof(ObservableValidatorPage), "ObservableValidator", "observable form validation validate data error inotify property changed propertychanging changing"),
+            new SampleEntry(CommandsItem, typeof(RelayCommandPage), "RelayCommand and RelayCommand<T>", "commands icommand relaycommand binding"),
+            new SampleEntry(AsyncCommandsItem, typeof(AsyncRelayCommandPage), "AsyncRelayCommand and AsyncRelayCommand<T>", "asynccommands icommand relaycommand binding asynchronous"),
+            new SampleEntry(MessengerItem, typeof(MessengerPage), "Messenger and IMessenger", "messenger messaging message receiver recipient"),
+            new SampleEntry(SendMessagesItem, typeof(MessengerSendPage), "[IMessenger] Send messages", "messenger messaging message receiver recipient send"),
+            new SampleEntry(RequestMessagesItem, typeof(MessengerRequestPage), "[IMessenger] Request messages", "messenger messaging message receiver recipient request reply"),
+            new SampleEntry(InversionOfControlItem, typeof(IocPage), "Ioc (Inversion of control)", "ioc inversion control dependency injection service locator"),
+            new SampleEntry(CollectionsItem, typeof(CollectionsPage), "Collections", "collection observable mvvm group list grid items source"),
+            new SampleEntry(RedditBrowserOverviewItem, typeof(PuttingThingsTogetherPage), "Putting things together"),
+            new SampleEntry(ViewModelsSetupItem, typeof(SettingUpTheViewModelsPage), "Setting up the ViewModels"),
+            new SampleEntry(SettingsServiceItem, typeof(SettingsServicePage), "Settings service"),
+            new SampleEntry(RedditServiceItem, typeof(RedditServicePage), "Reddit service"),
+            new SampleEntry(BuildingTheUIItem, typeof(BuildingTheUIPage), "Building the UI"),
+            new SampleEntry(FinalResultItem, typeof(RedditBrowserPage), "Reddit browser")
+        };*/
+
+            // Set the custom title bar to act as a draggable region
+            Window.Current.SetTitleBar(TitleBarBorder);
+        }
+
+        // Navigates to a sample page when a button is clicked
+        private void NavigationView_OnItemInvoked(NavigationView navigationView, NavigationViewItemInvokedEventArgs navigationViewItemInvokedEventArgs)
+        {
+            /*if (NavigationItems.FirstOrDefault(item => item.Item == navigationViewItemInvokedEventArgs.InvokedItemContainer)?.PageType is Type pageType)
+            {
+                NavigationFrame.Navigate(pageType);
+            }*/
+        }
+
+        // Sets whether or not the back button is enabled
+        private void NavigationFrame_OnNavigated(object sender, NavigationEventArgs e)
+        {
+            NavigationView.IsBackEnabled = ((Frame)sender).BackStackDepth > 0;
+        }
+
+        // Navigates back
+        private void NavigationView_OnBackRequested(Microsoft.UI.Xaml.Controls.NavigationView navigationView, NavigationViewBackRequestedEventArgs navigationViewBackRequestedEventArgs)
+        {
+            if (NavigationFrame.BackStack.LastOrDefault() is PageStackEntry entry)
+            {
+                NavigationView.SelectedItem = NavigationItems.First(item => item.PageType == entry.SourcePageType).Item;
+
+                NavigationFrame.GoBack();
+            }
+        }
+
+        // Select the introduction item when the shell is loaded
+        private void Shell_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            NavigationView.SelectedItem = SettingsItem;
+
+            /*NavigationFrame.Navigate(typeof(IntroductionPage));*/
+        }
+
+        // Updates the search results
+        private void SearchBox_OnTextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
+        {
+            if (args.Reason == AutoSuggestionBoxTextChangeReason.UserInput)
+            {
+                // Not a simple tokenized search, but good enough for now
+                string query = sender.Text.ToLowerInvariant();
+
+                sender.ItemsSource = NavigationItems.Where(item => item.Tags?.Contains(query) == true);
+            }
+        }
+
+        // Navigates to a selected item
+        private void AutoSuggestBox_OnSuggestionChosen(AutoSuggestBox sender, AutoSuggestBoxSuggestionChosenEventArgs args)
+        {
+            SampleEntry entry = (SampleEntry)args.SelectedItem;
+
+            NavigationFrame.Navigate(entry.PageType);
+
+            NavigationView.SelectedItem = entry.Item;
+
+            sender.Text = string.Empty;
+        }
+    }
+
+
+    /// <summary>
+    /// A simple model for tracking sample pages associated with buttons.
+    /// </summary>
+    public sealed class SampleEntry
+    {
+        public SampleEntry(NavigationViewItem viewItem, Type pageType, string? name = null, string? tags = null)
+        {
+            Item = viewItem;
+            PageType = pageType;
+            Name = name;
+            Tags = tags;
+        }
+
+        /// <summary>
+        /// The navigation item for the current entry.
+        /// </summary>
+        public NavigationViewItem Item { get; }
+
+        /// <summary>
+        /// The associated page type for the current entry.
+        /// </summary>
+        public Type PageType { get; }
+
+        /// <summary>
+        /// Gets the name of the current entry.
+        /// </summary>
+        public string? Name { get; }
+
+        /// <summary>
+        /// Gets the tag for the current entry, if any.
+        /// </summary>
+        public string? Tags { get; }
+    }
+
+}
